dbaa2b6c5c6491b8b72c8be6c970d0b4b7867a02
kal-kyokya <kalkyokya4@gmail.com>
2025-03-17 09:06:55 +0000

    Retrieve Video lists from backend using Axios.

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,14 +5,39 @@ import List from '../../components/list/List';
 import Watch from '../../pages/watch/Watch';
 import Register from '../../pages/register/Register';
 import Login from '../../pages/login/Login';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = ({ category }) => {
+    const [lists, setLists] = useState([]);
+    const [subCategory, setSubCategory] = useState([]);
+
+    useEffect(() => {
+	const getRandomLists = async () => {
+	    try {
+		const res = await axios.get(
+		    `lists${category ? '?category=' + category : ''}${
+		    subCategory ? '?subCategory=' + subCategory : ''
+		  }`,
+		  {
+		    headers: { token: '' }
+		  }
+		);
+	    } catch (err) {
+		console.log(err);
+	    }
+	};
+
+	getRandomLists();
+    }, [category, subCategory]);
+
     return (
 	<div className='home'>
 	    <Navbar />
 	    <Featured category={ category } />
-	    <List />
-	    <List />
+	    {lists.map((list) => {
+<List />
+	    })}
 	</div>
     );
 }
