8303905f4dc60e745ebab4f3452bf6316ff8de08
kal-kyokya <kalkyokya4@gmail.com>
2025-05-03 13:03:48 +0000

    Add an environment variable stored baseURL to axios requests.

--- frontend/src/components/list/List.jsx
+++ frontend/src/components/list/List.jsx
@@ -15,12 +15,12 @@ const List = ({ list }) => {
 
 	if (direction === 'left' && slideNumber > 0) {
 	    setSlideNumber(slideNumber - 1);
-	    listRef.current.style.transform = `translateX(${230 + distance}px)`;
+	    listRef.current.style.transform = `translateX(${299 + distance}px)`;
 	}
 
-	if (direction === 'right' && slideNumber < 5) {
+	if (direction === 'right' && slideNumber < 6) {
 	    setSlideNumber(slideNumber + 1);
-	    listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+	    listRef.current.style.transform = `translateX(${-299 + distance}px)`;
 	}
     }
 
