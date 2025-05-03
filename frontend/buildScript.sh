83893702a9c31902960c3ebc44c480a13ce505dc
kal-kyokya <kalkyokya4@gmail.com>
2025-05-03 08:04:41 +0000

    Complete creation and set up of time-saving shell scripts.

--- frontend/buildScript.sh
+++ frontend/buildScript.sh
@@ -6,4 +6,5 @@ sudo npm run build
 sudo cp -R dist/ /var/www/Futtech/frontend/
 sudo nginx -t
 sudo systemctl reload nginx
-echo "Build and Nginx reload: Completed"
+
+echo $'\nBuild and Nginx reload: Completed'
