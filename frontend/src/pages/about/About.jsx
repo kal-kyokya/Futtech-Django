0aab0399b79734586379f5a12105a125fb57e912
kal-kyokya <kalkyokya4@gmail.com>
2025-05-03 13:04:55 +0000

    Add an environment variable stored baseURL to axios requests.

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -48,10 +48,10 @@ const About = () => {
 		     alt="Futtech Register page"/>
 		<p>Futtech is a startup that helps football players and coaches gain tactical awareness of their strengths and weaknesses through online provision of drone footage and Artificial Intelligent targeted feedback.<br />
 		    As an engineering problem, It focuses on three objectives: creating a web and mobile-friendly experience, building cost-effective camera-equipped drones, and designing a reliable AI model.<br />
-		This is the <i>web and mobile-friendly</i> online platform, onto which drone footage is to be uploaded before AI analysis and feedback generation—<a href="http://www.futtech.kalkyokya.tech">Demo here</a>.</p>
+		This is the <i>web and mobile-friendly</i> online platform, onto which drone footage is to be uploaded before AI analysis and feedback generation—<a href="http://www.futtech.kalkyokya.tech" target="_blank">Demo here</a>.</p>
 
 		<h3>WHY FUTTECH</h3>
-		<p>Being a big fan of Football, this was an easy project focus to commit to. I am a hobbyist footballer, and recently learned that my father, and his father before him, played for the great <a href="https://en.wikipedia.org/wiki/TP_Mazembe#Honours">TP Mazembe</a> in Congo, DRC.<br />
+		<p>Being a big fan of Football, this was an easy project focus to commit to. I am a hobbyist footballer, and recently learned that my father, and his father before him, played for the great <a href="https://en.wikipedia.org/wiki/TP_Mazembe#Honours" target="_blank">TP Mazembe</a> in Congo, DRC.<br />
 		    Over a year of training sessions revealed a frustrating pattern: <b>tactical lessons often get lost</b> between sessions. Without tools to track movements, analyze positioning and replay my actions, <b>I repeated mistakes</b>.<br />
 		Instead of complaining, I decided to <b>build a solution</b>.</p>
 
@@ -63,7 +63,7 @@ const About = () => {
 
 		<h4>Technology Used</h4>
 		<p>Futtech is a MERN Stack Application.<br />
-		I chose this stack to grow my backend skills while learning React and <a href="https://www.w3schools.com/sass/sass_intro.asp">Sass</a> for building functional, scalable client-side interfaces.</p>
+		I chose this stack to grow my backend skills while learning React and <a href="https://www.w3schools.com/sass/sass_intro.asp" target="_blank">Sass</a> for building functional, scalable client-side interfaces.</p>
 
 		<h4>Notable Features</h4>
 		<ul>
@@ -77,7 +77,9 @@ const About = () => {
 		    In January 2025, I set up Futtech as its subdomain, but after weeks building it using MERN, deployment on my Ubuntu VPS presented challenges, delaying the initial timeline.<br />
 		    I invested two days intensively learning deployment through articles and YouTube tutorials. Armed with new knowledge, I successfully deployed Futtech using PM2, Nginx, SSL (Let's Encrypt), and proper directory management.<br /><br />
 		    <em>
-			Check <a href="https://medium.com/blockchain-research-lab-akgec/the-ultimate-beginners-guide-to-deploying-a-mern-stack-on-ubuntu-with-nginx-pm2-and-ssl-by-let-s-50d3d1c355ef">
+			Check <a href="https://medium.com/blockchain-research-lab-akgec/the-ultimate-beginners-guide-to-deploying-a-mern-stack-on-ubuntu-with-nginx-pm2-and-ssl-by-let-s-50d3d1c355ef"
+				 target="_blank"
+			      >
 				  this article
 			      </a> for a step-by-step deployment guide.
 		    </em>
@@ -86,14 +88,14 @@ const About = () => {
 		<h3>LESSONS LEARNED</h3>
 		<h4>Technical Takeaways</h4>
 		<p>The MERN stack offers a satisfying full-stack experience.<br />
-		<a href="https://www.w3schools.com/sass/sass_intro.asp">Sass</a> improved my frontend understanding, and working through deployment deepened my backend expertise.</p>
+		<a href="https://www.w3schools.com/sass/sass_intro.asp" target="_blank">Sass</a> improved my frontend understanding, and working through deployment deepened my backend expertise.</p>
 
 		<h4>What I Might Do Differently</h4>
 		<p>I would design automated test scripts to avoid overreliance on manual testing and debugging.</p>
 
 		<h4>What I Learned About Myself</h4>
 		<p>I am willing to put in long hours to master full-cycle software development.<br />
-		    Thanks to my foundational training with <a href="https://www.alxafrica.com/">
+		    Thanks to my foundational training with <a href="https://www.alxafrica.com/" target="_blank">
 								ALX Africa
 							    </a>, I value understanding systems from the ground up, not just using high-level abstractions.</p>
 
@@ -115,13 +117,13 @@ const About = () => {
 		— Jean-Paul De Marie KYOKYA Kalulu</p>
 
 		<p>
-		    <a href="https://github.com/kal-kyokya/Futtech">
+		    <a href="https://github.com/kal-kyokya/Futtech" target="_blank">
 			Link to GitHub for the project
 		    </a><br />
-		    <a href="https://futtech.kalkyokya.tech/">
+		    <a href="https://futtech.kalkyokya.tech/" target="_blank">
 			Link to the deployed project page
 		    </a><br />
-		    <a href="https://www.linkedin.com/in/jean-paul-kyokya-b21892223/">
+		    <a href="https://www.linkedin.com/in/jean-paul-kyokya-b21892223/" target="_blank">
 			Link to my LinkedIn profile
 		    </a>
 		</p>
