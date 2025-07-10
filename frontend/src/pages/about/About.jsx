698de131baad3752c9eac79372c8f91f30e80a29
kal-kyokya <kalkyokya4@gmail.com>
2025-07-10 10:11:38 +0000

    Implement link redirection best practice by addind 'ref=noopener noreferrer' to anchor tags.

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -38,7 +38,8 @@ const About = () => {
 		<div className="socials">
 		    <span>Detailed version:&ensp;</span>
 		    <a href="https://medium.com/@kal-kyokya/the-futtech-startup-a-journey-of-engineering-prototyping-debugging-entrepreneurship-e3bfb91d2de5"
-		       target="_blank">
+		       target="_blank"
+		    rel="noopener noreferrer">
 			<img alt="Medium badge"
 	   		     src="https://img.shields.io/badge/Medium-black?logo=medium&logoColor=white"/>
 		    </a>
@@ -55,10 +56,10 @@ const About = () => {
 		     alt="Futtech Register page"/>
 		<p>Futtech is a startup that helps football players and coaches gain tactical awareness of their strengths and weaknesses through online provision of drone footage and Artificial Intelligent targeted feedback.<br />
 		    As an engineering problem, It focuses on three objectives: creating a web and mobile-friendly experience, building cost-effective camera-equipped drones, and designing a reliable AI model.<br />
-		This is the <i>web and mobile-friendly</i> online platform, onto which drone footage is to be uploaded before AI analysis and feedback generation—<a href="http://www.futtech.kalkyokya.tech" target="_blank">Demo here</a>.</p>
+		This is the <i>web and mobile-friendly</i> online platform, onto which drone footage is to be uploaded before AI analysis and feedback generation—<a href="http://www.futtech.kalkyokya.tech" target="_blank" rel="noopener noreferrer">Demo here</a>.</p>
 
 		<h3>WHY FUTTECH</h3>
-		<p>Being a big fan of Football, this was an easy project focus to commit to. I am a hobbyist footballer, and recently learned that my father, and his father before him, played for the great <a href="https://en.wikipedia.org/wiki/TP_Mazembe#Honours" target="_blank">TP Mazembe</a> in Congo, DRC.<br />
+		<p>Being a big fan of Football, this was an easy project focus to commit to. I am a hobbyist footballer, and recently learned that my father, and his father before him, played for the great <a href="https://en.wikipedia.org/wiki/TP_Mazembe#Honours" target="_blank" rel="noopener noreferrer">TP Mazembe</a> in Congo, DRC.<br />
 		    Over a year of training sessions revealed a frustrating pattern: <b>tactical lessons often get lost</b> between sessions. Without tools to track movements, analyze positioning and replay my actions, <b>I repeated mistakes</b>.<br />
 		Instead of complaining, I decided to <b>build a solution</b>.</p>
 
@@ -70,7 +71,7 @@ const About = () => {
 
 		<h4>Technology Used</h4>
 		<p>Futtech is a MERN Stack Application.<br />
-		I chose this stack to grow my backend skills while learning React and <a href="https://www.w3schools.com/sass/sass_intro.asp" target="_blank">Sass</a> for building functional, scalable client-side interfaces.</p>
+		I chose this stack to grow my backend skills while learning React and <a href="https://www.w3schools.com/sass/sass_intro.asp" target="_blank" rel="noopener noreferrer">Sass</a> for building functional, scalable client-side interfaces.</p>
 
 		<h4>Notable Features</h4>
 		<ul>
@@ -83,10 +84,11 @@ const About = () => {
 		<p>I acquired the domain 'kalkyokya.tech' in July 2024.<br />
 		    In January 2025, I set up Futtech as its subdomain, but after weeks building it, deployment on my Ubuntu VPS presented challenges, delaying the initial timeline.<br />
 		    I invested two days intensively learning deployment through articles and YouTube tutorials. Armed with new knowledge, I successfully deployed Futtech using PM2, Nginx, SSL (Let's Encrypt).<br />
-		    This immediately gave rise to a new challenge; Cross-Origin Resource Sharing (<a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/CORS" target="_blank">CORS</a>). It took another two days to go through the documentation, stackoverflow and YouTube tutorials to understand the bugs.<br /><br />
+		    This immediately gave rise to a new challenge; Cross-Origin Resource Sharing (<a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/CORS" target="_blank" rel="noopener noreferrer">CORS</a>). It took another two days to go through the documentation, stackoverflow and YouTube tutorials to understand the bugs.<br /><br />
 		    <em>
 			Check <a href="https://medium.com/blockchain-research-lab-akgec/the-ultimate-beginners-guide-to-deploying-a-mern-stack-on-ubuntu-with-nginx-pm2-and-ssl-by-let-s-50d3d1c355ef"
 				 target="_blank"
+				 rel="noopener noreferrer"
 			      >
 				  this article
 			      </a> for a step-by-step deployment guide.
@@ -103,7 +105,7 @@ const About = () => {
 
 		<h4>What I Learned About Myself</h4>
 		<p>I am willing to put in long hours to master full-cycle software development.<br />
-		    Thanks to my foundational training with <a href="https://www.alxafrica.com/" target="_blank">
+		    Thanks to my foundational training with <a href="https://www.alxafrica.com/" target="_blank" rel="noopener noreferrer">
 								ALX Africa
 							    </a>, I value understanding systems from the ground up, not just using high-level abstractions.</p>
 
@@ -125,19 +127,19 @@ const About = () => {
 		— Jean-Paul De Marie KYOKYA Kalulu</p>
 
 		<p>
-		    <a href="https://github.com/kal-kyokya/Futtech" target="_blank">
+		    <a href="https://github.com/kal-kyokya/Futtech" target="_blank" rel="noopener noreferrer">
 			Link to GitHub for the project
 		    </a><br />
-		    <a href="https://futtech.kalkyokya.tech/" target="_blank">
+		    <a href="https://futtech.kalkyokya.tech/" target="_blank" rel="noopener noreferrer">
 			Link to the deployed project page
 		    </a><br />
-		    <a href="https://www.linkedin.com/in/jean-paul-kyokya-b21892223/" target="_blank">
+		    <a href="https://www.linkedin.com/in/jean-paul-kyokya-b21892223/" target="_blank" rel="noopener noreferrer">
 			Link to my LinkedIn profile
 		    </a><br />
-                    <a href="https://twitter.com/JPDemarie" target="_blank">
+                    <a href="https://twitter.com/JPDemarie" target="_blank" rel="noopener noreferrer">
                         Link to my X (Twitter) profile
                     </a><br />
-                    <a href="https://medium.com/@kal-kyokya/the-futtech-startup-a-journey-of-engineering-prototyping-debugging-entrepreneurship-e3bfb91d2de5" target="_blank">
+                    <a href="https://medium.com/@kal-kyokya/the-futtech-startup-a-journey-of-engineering-prototyping-debugging-entrepreneurship-e3bfb91d2de5" target="_blank" rel="noopener noreferrer">
                         Link to the full article on Medium
                     </a>
 		</p>
