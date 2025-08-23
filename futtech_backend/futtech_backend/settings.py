eb6591520ac5eacefb2372cc0dd7bb4ccdb50c5e
kal-kyokya <kalkyokya4@gmail.com>
2025-08-23 12:25:38 +0000

    Install the 'dotenv' python package enabling loading of environment variables.

--- futtech_backend/futtech_backend/settings.py
+++ futtech_backend/futtech_backend/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
