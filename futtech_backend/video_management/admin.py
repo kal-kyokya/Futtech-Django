dc3cc58dcf75070b1ed03c9dcf751f77829a01da
kal-kyokya <kalkyokya4@gmail.com>
2025-08-26 13:02:26 +0000

    Attempt to customize the display of the video management's models.

--- futtech_backend/video_management/admin.py
+++ futtech_backend/video_management/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+#!/usr/bin/env python3
+"""
+'video_management/admin.py' registers this App's models to the admin App
+and enables super user manipulation of said models in a browser tab.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import UserProfile, Video
+
+
+class VideoAdmin(admin.ModelAdmin):
+    """
+    Enables customization of the Video appearance on the admin page.
+
+    Inheritance:
+    	admin.ModelAdmin - Described as 'Encapsulating all admin options and
+    	functionality for a given model', here allows declaration of desired
+    	fields to be displayed on the admin page.
+    """
+
+    list_display = ['title', 'created_at']
+
+
+admin.site.register([UserProfile, VideoAdmin, Video])
