ada701925d17261b613e63cb65e7099020e34c38
kal-kyokya <kalkyokya4@gmail.com>
2025-08-27 08:29:29 +0000

    Resolve model's admin subclass configuration bug disabling customization of the admin page.

--- futtech_backend/video_management/admin.py
+++ futtech_backend/video_management/admin.py
@@ -8,6 +8,7 @@ from django.contrib import admin
 from .models import UserProfile, Video
 
 
+@admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
     """
     Enables customization of the Video appearance on the admin page.
@@ -18,7 +19,21 @@ class VideoAdmin(admin.ModelAdmin):
     	fields to be displayed on the admin page.
     """
 
-    list_display = ['title', 'created_at']
+    list_display = ('title', 'owner', 'description', 'created_at')
 
 
-admin.site.register([UserProfile, VideoAdmin, Video])
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Enables customization of the UserProfile appearance on the admin page.
+
+    Inheritance:
+    	admin.ModelAdmin - Described as 'Encapsulating all admin options and
+    	functionality for a given model', here allows declaration of desired
+    	fields to be displayed on the admin page.
+    """
+
+    list_display = ('__str__', 'bio')
+
+
+# admin.site.register([UserProfile, VideoAdmin, Video])
