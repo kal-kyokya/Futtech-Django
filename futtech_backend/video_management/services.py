7c79266f02a0e6b80f07354b24652a51a10d3d7f
kal-kyokya <kalkyokya4@gmail.com>
2025-08-27 12:53:29 +0000

    Implement logic creating Mux's direct upload URL.

--- futtech_backend/video_management/services.py
+++ futtech_backend/video_management/services.py
@@ -8,6 +8,7 @@ import os
 import mux_python
 from .models import Video
 from dotenv import load_dotenv
+import logger
 
 
 # --------------------------
@@ -21,10 +22,42 @@ configuration = mux_python.Configuration()
 configuration.username = os.environ.get('MUX_TOKEN_ID')
 configuration.password = os.environ.get('MUX_TOKEN_SECRET')
 
-# API clients
-assets_api = mux_python.AssetsApi(
+# API Client Initialization
+uploads_api = mux_python.DirectUploadsApi(
     mux_python.ApiClient(
         configuration
     )
 )
 
+def create_direct_upload_url():
+    """
+    Creates a new direct upload URL in Mux.
+
+    Return:
+    	A URL passed to the client to upload the file directly to Mux.
+    """
+
+    create_asset_request = mux_python.CreateAssetRequest(
+        playback_policy=[
+            mux_python.PlaybackPolicy.PUBLIC,
+        ]
+    )
+    create_upload_request = mux_python.CreateUploadRequest(
+        timeout=3600,
+        new_asset_settings=create_asset_request,
+        cors_origin='https://futtech.kalkyokya.tech'
+    )
+
+    try:
+        create_upload_response = uploads_api.create_direct_upload(
+            create_upload_request
+        )
+        logger.print_debug(str(create_upload_response))
+
+        assert create_upload_response != None
+        assert create_upload_response.data != None
+        assert create_upload_response.data.id != None
+
+        return create_upload_response.data
+    except mux_python.ApiException as err:
+        pass
