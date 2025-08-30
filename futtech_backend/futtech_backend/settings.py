9e378a907336d6b70b5d5ad114081801fb3529a6
kal-kyokya <kalkyokya4@gmail.com>
2025-08-30 08:49:48 +0000

    Change the Django Project Secret Key and add it to the environment.

--- futtech_backend/futtech_backend/settings.py
+++ futtech_backend/futtech_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a(gv-h-7rkj%=i)of8pges*-w$mza%)2*48dtsbugemf@lv%$k'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
