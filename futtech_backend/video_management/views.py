bf6c576ba29bc44f4177b8ba57253fed08bcfb3d
kal-kyokya <kalkyokya4@gmail.com>
2025-08-30 11:52:11 +0000

    Complete creation of the view function handling requests for playback IDs.

--- futtech_backend/video_management/views.py
+++ futtech_backend/video_management/views.py
@@ -6,6 +6,7 @@ Business logic and Data layer, for defined set of URLs.
 
 from django.http import JsonResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from .logs import logger
 from .models import Video
 from . import services
 
@@ -22,3 +23,26 @@ def get_playback_token(request, video_id):
     Return:
     	A JSON Web Token containing a signed version of the playback ID.
     """
+
+    try:
+        video = Video.objects.get(pk=video_id)
+    except Video.DoesNotExist as err:
+        logger.error("Error retrieving video ID - {} from DB: {}".format(
+            video_id, err
+        ))
+
+    # Later on, we shall add subscription checks here.
+    # For now, we just check if the user is logged in.
+    if not video.is_premium or request.user.is_authenticated:
+        token = services.generate_signed_playback_token(
+            video.mux_playback_id
+        )
+        if token:
+            return JsonResponse({'token': token})
+        else:
+            return JsonResponse(
+                {'error': 'Could not generate token'}, status=500
+            )
+
+    logger.info("Unauthorized request for a playback ID.")
+    return HttpResponseForbidden("You do not have permission to view this video.")
