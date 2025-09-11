4b78b6d7d9ca1415f53667e0483581fa35d760e5
kal-kyokya <kalkyokya4@gmail.com>
2025-09-11 11:18:47 +0000

    Implement the data logic needed by the video management's view to handle newly created urls.

--- futtech_backend/video_management/views.py
+++ futtech_backend/video_management/views.py
@@ -6,19 +6,49 @@ Business logic and Data layer, for defined set of URLs.
 
 from django.http import JsonResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from djstripe.settings import djstripe_settings
 from .logs import logger
 from .models import Video
 from . import services
 
 
 @login_required
-def get_playback_token(request, video_id):
+def get_video_data(request, video_id):
     """
-    Handles HTTP requests for Mux video assets ready for consumption.
+    Handles HTTP requests for Mux video assets.
 
     Params:
     	request - A dictionary object representing the client's request.
-    	video_id - A string representing the Mux asset ID requested.
+    	video_id - A string representing the requested Mux asset ID.
+
+    Return:
+    	A JSON Web Token containing a signed version of the playback ID.
+    """
+
+    try:
+        video = Video.objects.get(pk=video_id)
+    except Video.DoesNotExist as err:
+        logger.error("Error retrieving video ID - {} from DB: {}".format(
+            video_id, err
+        ))
+
+    # We check if the user is logged in.
+    if not video.is_premium or request.user.is_authenticated:
+        return JsonResponse({'data': video})
+    else:
+        logger.info("Unauthorized request for a playback ID.")
+        return HttpResponseForbidden("You do not have permission to view this video.")
+
+
+@login_required
+def get_playback_token(request, video_id):
+    """
+    Handles HTTP requests for playback-ready Mux video assets.
+
+    Params:
+    	request - A dictionary object representing the client's request.
+    	video_id - A string representing the requested Mux asset ID.
 
     Return:
     	A JSON Web Token containing a signed version of the playback ID.
@@ -46,3 +76,22 @@ def get_playback_token(request, video_id):
 
     logger.info("Unauthorized request for a playback ID.")
     return HttpResponseForbidden("You do not have permission to view this video.")
+
+
+@login_required
+def get_pricing_page_identifiers(request):
+    """
+    Provides the keys and IDs required for usage of Stripe's pricing table.
+
+    Params:
+    	request - Dictionary object containing client-side data needed to
+    		  effectively respond to the user's request.
+
+    Return:
+    	A JSON object bundling together all necessary identifiers.
+    """
+
+    return JSONResponse({
+        'stripe_public_key': djstripe_settings.STRIPE_PUBLIC_KEY,
+        'stripe_pricing_table_id': settings.STRIPE_PRICING_TABLE_ID
+    })
