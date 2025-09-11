bfcfa775cb98a3cafea2546d7b58da82fd198802
kal-kyokya <kalkyokya4@gmail.com>
2025-09-11 08:51:11 +0000

    Ensure the Payment component is React-safe.

--- frontend/src/components/payment/Payment.jsx
+++ frontend/src/components/payment/Payment.jsx
@@ -1,11 +1,18 @@
-import useState from 'react';
+import { useEffect, useRef } from 'react';
 
-const Payment = () => {
+const Payment = ({ pricingTableId, publishableKey }) => {
+    const ref = useRef();
+
+    useEffect(() => {
+	if (!ref.current) return;
+
+	// Set attributes manually to be React-safe
+	ref.current.setAttribute('pricing-table-id', pricingTableId);
+	ref.current.setAttribute('publishable-key', publishableKey);
+    }, [pricingTableId, publishableKey]);
 
     return (
-	    <stripe-pricing-table
-	pricing-table-id="prctbl_1S65hV2MjVhxhquqdN0DOFud"
-	publishable-key="pk_test_51S53OX2MjVhxhquqEU1g6JQYl80fZfUhrvEjsUnnxqK9nTS6ymTX1zPgBLFmwryPMSjknHuSe1gqJti6WZE2WVtI009w1KduwH">
+	    <stripe-pricing-table ref={ref}>
 	    </stripe-pricing-table>
     );
 };
