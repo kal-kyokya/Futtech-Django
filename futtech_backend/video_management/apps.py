78b0f3c3b7e0350023aaadbeb3454705ad685471
kal-kyokya <kalkyokya4@gmail.com>
2025-09-15 09:03:59 +0000

    Ensure the signal module is imported within the video management App config file.

--- futtech_backend/video_management/apps.py
+++ futtech_backend/video_management/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class VideoManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'video_management'
+
+    def ready(self):
+        import video_management.signals
