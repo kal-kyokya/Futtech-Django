5b309f2fd800dccc2161122d58215e0deb450685
kal-kyokya <kalkyokya4@gmail.com>
2025-09-17 12:15:09 +0000

    Refactor the authentication API calls to hit Django and not Express.

--- frontend/src/contexts/authContext/apiCalls.js
+++ frontend/src/contexts/authContext/apiCalls.js
@@ -2,22 +2,40 @@ import axios from 'axios';
 import { loginStart, loginSuccess, loginFailure } from './AuthActions';
 import { useNavigate } from 'react-router-dom';
 
-const login = async (user, dispatch) => {
+
+// Re-using the same conceptual API service as in '../newVideo/NewVideo'
+export const apiService = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    interceptors: {
+	request: (config) => {
+	    const user = JSON.parse(localStorage.getItem('user'));
+	    if (user && user.access_token) {
+		config.headers.Authorization = `Bearer ${user.access_token}`;
+	    }
+	    return config;
+	},
+    },
+});
+
+const login = async (userCredentials, dispatch) => {
     dispatch(loginStart());
 
     try {
 	const res = await axios.post(
-	    'http:/127.0.0.1:8080/auth/signIn',
-	    user,
-	    { headers: {'content-type': 'application/json'} }
+	    '${import.meta.env.VITE_API_BASE_URL}/auth/login/',
+	    userCredentials
 	);
 
-	dispatch(loginSuccess(res.data));
-    } catch (err) {
-	console.log(err);
-    }
+	if (res.data && res.data.access_token) {
+	    localStorage.setItem('user', JSON.stringigy(res.data));
+	    dispatch(loginSuccess(res.data));
+	} else {
+	    dispatch(loginFailure());
+	}
 
-    dispatch(loginFailure());
+    } catch (err) {
+	dispatch(loginFailure());
+    }
 };
 
 export default login;
