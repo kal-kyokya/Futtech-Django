ef39ac74dcf6fb27a0f2b0ee9aeefa3037ca3afe
kal-kyokya <kalkyokya4@gmail.com>
2025-09-19 08:00:11 +0000

    Document the 'serializers.py' module of the 'user_auth' app.

---
+++ futtech_backend/user_auth/serializers.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+'serializers.py' contains python classes defining process through which input
+		 data should be validated before creation of Django-friendly
+		 objects, like Django Model fields for example.
+"""
