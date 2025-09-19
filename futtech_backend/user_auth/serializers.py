7ea72382c677d0b05b653660cb10ce8f85ae0c64
kal-kyokya <kalkyokya4@gmail.com>
2025-09-19 09:00:05 +0000

    Document the User registration serializer custom class.

--- futtech_backend/user_auth/serializers.py
+++ futtech_backend/user_auth/serializers.py
@@ -9,3 +9,14 @@ import django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """
+    Handles validation of the user data sent as request body as well as
+    the creation of Django compliant complex python objects (Model fields)
+
+    Inheritance:
+    	serializers.ModelSerializer - Avail rest_framework's out-of-the-box
+    	features enabling data validation and serialization/deserialization.
+    """
