47706f6d4744d5df1c81cff991be485d0b0d51b3
kal-kyokya <kalkyokya4@gmail.com>
2025-09-19 10:22:35 +0000

    Define the serializer configuration via the 'Meta' class.

--- futtech_backend/user_auth/serializers.py
+++ futtech_backend/user_auth/serializers.py
@@ -40,3 +40,28 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         write_only=True,
         style={'input_type': 'password'}
     )
+
+    class Meta:
+        """
+        Central place for configuration of the manner in which this serializer
+        instance of 'ModelSerializer' interacts with its associated Django
+        model.
+
+        Also defines how data is to be presented and handled during/after
+        serialization and deserialization.
+        """
+
+        # Django model upon which this serializer primarily acts
+        model = get_user_model()
+
+        # Model fields to include during serialization and deserialization
+        fields = ('email', 'username', 'password', 'password2')
+
+        # Additional configuration not directly tied to model fields
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'required': True,
+            }
+        }
+
