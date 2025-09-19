71b5424de1a5a20b1b0c858f6852c72095b680e8
kal-kyokya <kalkyokya4@gmail.com>
2025-09-19 11:52:34 +0000

    Implement the business logic processing user registration requests.

--- futtech_backend/user_auth/views.py
+++ futtech_backend/user_auth/views.py
@@ -23,3 +23,39 @@ class UserRegistrationView(generics.CreateAPIView):
     	POST method, serialzer instantiation, validation, saving, and
     	response generation.
     """
+
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny,)
+
+    # Override the 'create' method to include a JWT access and refresh tokens
+    def create(self, request, *args, **kwargs):
+        """
+        Customize the HTTP response to include a JWT access & refresh tokens.
+
+        Params:
+        	self - A representation of the current class instance.
+        	request - The client-side HTTP request made.
+
+        Return:
+        	A 'rest_framework.response.Response' object.
+        """
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        response_data = {
+            'message': 'User registered successfully',
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(
+            response_data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
