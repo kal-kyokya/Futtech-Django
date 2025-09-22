47b53d93bc2abbdf8b0b96062e88e26d979110c4
kal-kyokya <kalkyokya4@gmail.com>
2025-09-22 11:58:33 +0000

    Define the logic handling client-side requests to log in.

--- futtech_backend/user_auth/views.py
+++ futtech_backend/user_auth/views.py
@@ -88,4 +88,10 @@ class UserLoginView(APIView):
         	refresh tokens associated with the requesting user.
         """
 
-        
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(
+            serializer.validated_data,
+            status=status.HTTP_200_OK
+        )
