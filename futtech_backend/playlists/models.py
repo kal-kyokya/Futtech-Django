da613a7c663f79018475471eebe31b786faff089
kal-kyokya <kalkyokya4@gmail.com>
2025-09-22 12:18:47 +0000

    Document the 'models.py' file of the playlists app.

--- futtech_backend/playlists/models.py
+++ futtech_backend/playlists/models.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+"""
+'models.py' contains definition of the Django models required for
+	    effective CRUD operations on Futtech's playlists.
+"""
+
 from django.db import models
 
-# Create your models here.
+
