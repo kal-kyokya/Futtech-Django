f814004f20bbcee440a57dfec42c672fee88645f
kal-kyokya <kalkyokya4@gmail.com>
2025-09-23 06:43:37 +0000

    Define the logic enabling playlist creation.

--- futtech_backend/playlists/views.py
+++ futtech_backend/playlists/views.py
@@ -32,3 +32,16 @@ class PlaylistViewSet(viewsets.ModelViewSet):
         	A Django model query set containing public playlists.
         """
         return Playlist.objects.filter(is_public=True)
+
+    def perform_create(self, serializer):
+        """
+        Handles POST request creating new playlists.
+
+        Params:
+        	self - Object representation of the class instanciation.
+        	serializer - A validated Django model friendly data set.
+
+        Return:
+        	None. A side effect, in the creation of a playlist.
+        """
+        serializer.save(owner=self.request.user)
