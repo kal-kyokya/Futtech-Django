1d24b82c4d34a42ffc5184606a607a870efbd994
kal-kyokya <kalkyokya4@gmail.com>
2025-10-01 11:38:33 +0000

    Document the 'TokenService' class using JSDoc comments.

--- frontend/src/services/tokenService.js
+++ frontend/src/services/tokenService.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 /**
+ * 'tokenService.js' uses an hybrid approach to create a secure token storage
+ * 		     mechanism that uses both in-memory and web storage.
+ */
+
+
+/**
+ * @class
  *
- *
- **/
+ * 'TokenService' bundles together the attributes methods required for
+ * 		  successful creation of a secure token storage mechanism.
+ */
+class TokenService {
+
+}
