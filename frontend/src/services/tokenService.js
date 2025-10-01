00a82c05d3d94c8b17071aff80d9fdabc6aefe42
kal-kyokya <kalkyokya4@gmail.com>
2025-10-01 12:01:24 +0000

    Create a class handling secure JWT token storage mechanisms.

--- frontend/src/services/tokenService.js
+++ frontend/src/services/tokenService.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /**
- * 'tokenService.js' uses an hybrid approach to create a secure token storage
- * 		     mechanism that uses both in-memory and web storage.
+ * 'tokenService.js' creates a secure token storage mechanism
+ * 		     that solely uses in-memory (RAM) storage.
  */
 
 
@@ -13,4 +13,38 @@
  */
 class TokenService {
 
+    // Creates an instance of the TokenService class.
+    constructor() {
+	this.accessToken = null; // Keep in memory for security
+    }
+
+    /**
+     * Handles WRITE operations on the Access token (in-memory only).
+     *
+     * @param {string} token - The backend-generated access token.
+     */
+    setAccessToken() {
+	this.accessToken = token;
+    }
+
+    /**
+     * Handles READ operations on the Access token.
+     *
+     * @return {string} the Access token tied to this class instanciation.
+     */
+    getAccessToken() {
+	return this.accessToken;
+    }
+
+    // Checks if the Access token exists
+    hasTokens() {
+	return !!(this.accessToken);
+    }
+
+    // Clears the Access token (logout)
+    clearAccessToken() {
+	this.accessToken = null;
+    }
 }
+
+export default new TokenService();
