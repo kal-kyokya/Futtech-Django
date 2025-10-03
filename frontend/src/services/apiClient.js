46b47a06783bc5d55ca7d17a5dcb7127571177e0
kal-kyokya <kalkyokya4@gmail.com>
2025-10-03 07:46:05 +0000

    Create the Axios instance to be used application-wide for API calls.

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -4,3 +4,17 @@
  *		  an Axios instance with interceptors to automatically handle
  * 		  token refresh.
  */
+
+import axios from 'axios';
+import tokenService from './tokenService';
+
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// Create the Axios instance
+const apiClient = axios.create({
+    baseUrl: API_BASE_URL,
+    headers: {
+	'Content-Type': 'application/json',
+    }
+});
