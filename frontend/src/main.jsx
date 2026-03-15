eb4b66448479ea3a4b7de4be041647e26ed5b68f
kal-kyokya <kalkyokya4@gmail.com>
2026-03-15 17:04:59 +0000

    Wrap the React app with the customized branding provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,17 +5,20 @@ import { AuthContextProvider } from './contexts/authContext/AuthContext.jsx';
 import { UserContextProvider } from './contexts/userContext/UserContext.jsx';
 import { VideoContextProvider } from './contexts/videoContext/VideoContext.jsx';
 import { PlaylistContextProvider } from './contexts/playlistContext/PlaylistContext.jsx';
+import { BrandingProvider } from './contexts/BrandingContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <AuthContextProvider>
-	  <UserContextProvider>
-	      <VideoContextProvider>
-		  <PlaylistContextProvider>
-		      <App />
-		  </PlaylistContextProvider>
-	      </VideoContextProvider>
-	  </UserContextProvider>
-      </AuthContextProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+	<BrandingProvider>
+	    <AuthContextProvider>
+		<UserContextProvider>
+		    <VideoContextProvider>
+			<PlaylistContextProvider>
+			    <App />
+			</PlaylistContextProvider>
+		    </VideoContextProvider>
+		</UserContextProvider>
+	    </AuthContextProvider>
+	</BrandingProvider>
+    </React.StrictMode>,
 )
