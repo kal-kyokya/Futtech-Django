067ee140379b6e320fa96f1e706e77d896e038ce
kal-kyokya <kalkyokya4@gmail.com>
2026-03-15 17:36:28 +0000

    Create the Tenant model inheriting from 'django.db.models'

--- futtech_backend/tenants/models.py
+++ futtech_backend/tenants/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tenant(models.Model):
+    slug = models.SlugField(max_length=100, unique=True)
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='logos/', blank=True, null=True)
+    primary_color = models.CharField(max_length=7, default='#028ECA')
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
