9b03f7e52459f69664c9074305247dae11841318
kal-kyokya <kalkyokya4@gmail.com>
2026-03-17 09:16:46 +0000

    Use 'git revert' to undo implementation of a multi-tenant architecture

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,20 +5,17 @@ import { AuthContextProvider } from './contexts/authContext/AuthContext.jsx';
 import { UserContextProvider } from './contexts/userContext/UserContext.jsx';
 import { VideoContextProvider } from './contexts/videoContext/VideoContext.jsx';
 import { PlaylistContextProvider } from './contexts/playlistContext/PlaylistContext.jsx';
-import { BrandingProvider } from './contexts/BrandingContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-	<BrandingProvider>
-	    <AuthContextProvider>
-		<UserContextProvider>
-		    <VideoContextProvider>
-			<PlaylistContextProvider>
-			    <App />
-			</PlaylistContextProvider>
-		    </VideoContextProvider>
-		</UserContextProvider>
-	    </AuthContextProvider>
-	</BrandingProvider>
-    </React.StrictMode>,
+  <React.StrictMode>
+      <AuthContextProvider>
+	  <UserContextProvider>
+	      <VideoContextProvider>
+		  <PlaylistContextProvider>
+		      <App />
+		  </PlaylistContextProvider>
+	      </VideoContextProvider>
+	  </UserContextProvider>
+      </AuthContextProvider>
+  </React.StrictMode>,
 )
