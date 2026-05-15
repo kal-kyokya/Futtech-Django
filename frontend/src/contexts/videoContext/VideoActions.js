98f8fa68ca003d97134a58624f4ae395c558207d
kal-kyokya <kalkyokya4@gmail.com>
2026-05-15 14:52:36 +0000

    Update the video context's actions to reflect deletion of the frontend video upload workflow

--- frontend/src/contexts/videoContext/VideoActions.js
+++ frontend/src/contexts/videoContext/VideoActions.js
@@ -1,22 +1,7 @@
 /**
  * A collection of callbacks returning objects
- * tracking the stages of video CRUD operations.
+ * tracking the stages of video retrieval operations.
  */
-
-// CREATE
-
-export const createVideoStart = () => ({
-    type: 'CREATE_VIDEO_START',
-});
-
-export const createVideoSuccess = (video) => ({
-    type: 'CREATE_VIDEO_SUCCESS',
-    payload: video,
-});
-
-export const createVideoFailure = () => ({
-    type: 'CREATE_VIDEO_FAILURE',
-});
 
 // GET
 
@@ -31,34 +16,4 @@ export const getVideosSuccess = (videos) => ({
 
 export const getVideosFailure = () => ({
     type: 'GET_VIDEOS_FAILURE',
-});
-
-// UPDATE
-
-export const updateVideoStart = () => ({
-    type: 'UPDATE_VIDEO_START',
-});
-
-export const updateVideoSuccess = (video) => ({
-    type: 'UPDATE_VIDEO_SUCCESS',
-    payload: video,
-});
-
-export const updateVideoFailure = () => ({
-    type: 'UPDATE_VIDEO_FAILURE',
-});
-
-// DELETE
-
-export const deleteVideoStart = () => ({
-    type: 'DELETE_VIDEO_START',
-});
-
-export const deleteVideoSuccess = (videoId) => ({
-    type: 'DELETE_VIDEO_SUCCESS',
-    payload: videoId,
-});
-
-export const deleteVideoFailure = () => ({
-    type: 'DELETE_VIDEO_FAILURE',
 });
