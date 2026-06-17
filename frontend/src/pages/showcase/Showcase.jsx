0f458ece3c35542917a0161277a8927ccb1ec909
kal-kyokya <kalkyokya4@gmail.com>
2026-06-17 08:41:44 +0000

    Set a new Email through which Futtech is to be contacted on, in the public showcase

--- frontend/src/pages/showcase/Showcase.jsx
+++ frontend/src/pages/showcase/Showcase.jsx
@@ -44,7 +44,7 @@ const Showcase = () => {
 		    </Link>
 		    <a
 			className='link heroAction secondary'
-			href='mailto:hello@futtech.io'
+			href='mailto:kalkyokya7@gmail.com'
 		    >
 			Contact Futtech
 		    </a>
