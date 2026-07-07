78b5bc980dbc8a64f091c565cd961409a256cf35
kal-kyokya <kalkyokya4@gmail.com>
2026-07-07 07:42:07 +0000

    Add the custom throttle class to the user auth views module

--- futtech_backend/user_auth/views.py
+++ futtech_backend/user_auth/views.py
@@ -10,6 +10,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
+from futtech_backend.throttles import LoginRateThrottle
 
 from django.conf import settings
 
@@ -119,6 +120,7 @@ class ObtainTokenCookieView(APIView):
 
     permission_classes = (AllowAny,)
     serializer_class = UserLoginSerializer
+    throttle_classes = [LoginRateThrottle]
 
     def post(self, request, *args, **kwargs):
         """
